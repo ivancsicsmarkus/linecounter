db03ae908eb7d363d0d827764db17de21aad3eea
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-08-19 20:32:51 +0200

    recursion && default ignored files

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ function counter(file) {
 	});
 }
 
-const ignore = process.argv[2].split(",");
+const ignore = {
+	user: process.argv[2] && process.argv[2].split(","),
+	default: ["node_modules", "package.json", "package-lock.json"]
+}
 
-fs.readdir("./", function(err, files) {
-	files.forEach((path) => {
-		// .git, .DS_Store...
-		if (path[0] === ".") {
-			return;
-		}
-		// it is a directory, recursion happens
-		else if (fs.statSync(path).isDirectory()) {
-			return;
-		}
-		// the file must be ignore
-		else if (ignore && ignore.includes(path)) {
-			return;
-		}
-		// it is a file
-		else {
-			counter(path);
-		}
-	});
-})
+function readDirectory(dir) {
+	dir = dir ? dir + "/" : "./";
+	fs.readdir(dir, function(err, files) {
+		files.forEach((path) => {
+			// .git, .DS_Store...
+			if (path[0] === ".") {
+				return;
+			}
+			// it is a directory, recursion happens
+			else if (fs.statSync(dir + path).isDirectory()) {
+				readDirectory(dir + path);
+			}
+			// the file must be ignored (by the user)
+			else if (ignore.user && ignore.user.includes(path)) {
+				return;
+			}
+			// the files must be ignored (by default)
+			else if (ignore.default.includes(path)) {
+				return;
+			}
+			// it is a file
+			else {
+				counter(dir + path);
+			}
+		});
+	})
+}
+
+readDirectory();
